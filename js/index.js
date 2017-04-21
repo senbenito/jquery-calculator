2d82526f4cfc60b579a8af015e354e7145bb7f42
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-21 10:05:49 -0600

    working through equals calculation - order of operations if's

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(function() {
 
   for (let i = 0; i < buttons.children.length; i++) {
     var buttonChildren = buttons.children[i];
-    console.log(buttonChildren);
+    // console.log(buttonChildren);
     if (buttonChildren.className !== "operator") {
       buttonChildren.setAttribute("id", buttonChildren.innerHTML);
     }
@@ -15,7 +15,8 @@ $(function() {
   $('#buttons-container').click(function(event) {
     if ((event.target).id === 'equals'){
     var screenTotal = $("#screen").html();
-    // console.log(typeof screenTotal);
+    console.log(screenTotal);
+    console.log(operatorStorage);
     totalIt(screenTotal);
   } else if ((event.target).className === "operator"){
         let operator = (event.target).innerHTML;
@@ -38,32 +39,31 @@ $(function() {
             break;
           default:
             $("#screen").html("");
+            operatorStorage = [];
         }
     } else {
       $("#screen").append((event.target).innerHTML);
     }
   });
 
+// "7x5+2"
+// ["x", "+"]
+
   function totalIt(string){
-    if (operatorStorage.length > 1){
-      let firstOperatorIndex = string.indexOf(operatorStorage[0]);
-      let firstNumber = string.slice(0,firstOperatorIndex);
-      let lastNumber = string.slice(firstOperatorIndex+1);
-      return firstNumber + operatorStorage[0] + lastNumber;
-    } else {
-
-    }
-}
-
-
-    let firstOperatorIndex = string.indexOf(operator);
-    if (string.indexOf(firstOperatorIndex+1, operator) === -1){
-      string.substring(firstOperatorIndex)
-    }
-    let lastOperator = string.indexOf(firstOperatorIndex+1, operator);
-    let computeThis = string.substring(firstOperatorIndex, lastOperator);
-
-  }
-
-
-});
+    var subtotalHolder = '';
+      for (let i=0; i<operatorStorage.length; i++){
+        if (operatorStorage.includes("x") || operatorStorage.includes("÷")){
+          console.log("multiply or divide!");
+          let multiplyIndex = string.indexOf("x");
+          let divideIndex = string.indexOf("÷");
+        } else {
+          console.log("add or subtract");
+        }
+        // let firstOperatorIndex = string.indexOf(operatorStorage[i]);
+        // let nextOperatorIndex = string.indexOf(operatorStorage[i+1]);
+        // let firstNumber = string.slice(0, firstOperatorIndex);
+        // let nextNumber = string.slice(firstOperatorIndex+1, nextOperatorIndex);
+      } // closes for loop
+      // return firstNumber + operatorStorage[0] + lastNumber;
+    } //closes totalIt
+});//closes JS
